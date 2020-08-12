98f9e5efaa2c4bfde3a38a2b2a0cb2d1056726e6
maktufkuatno <matufemsen61@gmail.com>
2020-08-13 00:55:23 +0700

    praktek pertemuan 10

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset-"UTF-8">
+    <meta name-"viewport content-"width-device-width, initial-sclae-1.0">
+    <title>{{@yield('title')}}<title>
+    <link rel-"stylesheet" href-"{{asset('css/app.css)}}">
+    <script src-"{{asset('js/app.js)}}"></script>
+<heaad>
+<body>
+    @include('layouts.navbar');
+    <h1>test halaman bootstrap</h1>
+</body>
+</html>
---
+++ resources/views/mahasiswa/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'halaman mahasiswa')
+
+@endsection
+
+@section('content')
+    <p>this is my body content.</p>
+@endselection
